933dccb64c26a5cab645a9684bb548a6c0af9cb7
Alan Harper <alan@aussiegeek.net>
2011-08-20 10:45:13 +1000

    Initial Commit

---
+++ FlickrSavr/FlickrSavrView.m
@@ -0,0 +1,52 @@
+//
+//  FlickrSavrView.m
+//  FlickrSavr
+//
+//  Created by Alan Harper on 20/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "FlickrSavrView.h"
+
+@implementation FlickrSavrView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
